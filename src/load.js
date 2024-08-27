25cad642a31af1412f734030c21ae310102ffe12
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-28 01:58:05 +0200

    Load title prompt

--- src/load.js
+++ src/load.js
@@ -1,7 +1,19 @@
+import bgImg from"../media/bg.jpg"
+
 export function loadDefault() {
     document.body.textContent = "";
     const main = document.createElement("div");
+    const bg = document.createElement("img");
+    const title = document.createElement("h1");
 
+    bg.src = bgImg;
+    bg.className = "bg";
+    title.textContent = "Search weather of:"
+    title.className = "title"
     main.className = "default";
+    main.style.zIndex = 1;
+
+    main.appendChild(title);
+    document.body.appendChild(bg);
     document.body.appendChild(main);
 }
