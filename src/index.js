f113dccfb6e0869e52fe465c4e079035f809d06c
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-29 00:19:09 +0200

    Get all important weather data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { getWeather } from "./external";
 import { SearchDiv } from "./load.js"
 import { format } from "date-fns";
 
+const NEXT_DAYS_NUM = 5;
+
 SearchDiv.getInput().addEventListener("keypress", (e) => {
     if (e.key === "Enter") loadingController(SearchDiv.getInputValue())
 })
@@ -11,27 +13,51 @@ SearchDiv.getBtn().addEventListener("click", () => loadingController(SearchDiv.g
 async function loadingController (obj) {
     const data = await getWeather(obj);
     const obtained = new GetData(data);
-    console.log(obtained.getTemp())
+    console.log(obtained.getTheme());
 }
 
 class GetData {
     constructor (data) {
-        this.data = data
+        this.data = data;
+        this.currDate = new Date(this.data.days[0].datetime);
+    }
+    getTheme () {
+        let icon = this.data.currentConditions.icon.split("-");
+        if (icon.length < 3) icon = icon[0];
+        else icon = icon[1];
+        return icon
+        
     }
     getMain () {
         const address = this.data.resolvedAddress.split(", ") 
         const title = address[0];
         const subTitle = address.slice(1).join(", ");
-        const displayDate = format(new Date(), "do MMMM, yyyy")
+        const displayDate = format(this.currDate, "do MMMM, yyyy")
         const description = this.data.description;
         return { title, subTitle, displayDate, description }
     }
     getTemp () {
         const currTemp = this.data.currentConditions.temp;
-        const maxTemp = this.data.days[0].tempmax;
-        const minTemp = this.data.days[0].tempmin;
+        const currMaxTemp = this.data.days[0].tempmax;
+        const currMinTemp = this.data.days[0].tempmin;
         const currFeel = this.data.currentConditions.feelslike;
 
-        return { currTemp, maxTemp, minTemp, currFeel}
+        return { currTemp, currMaxTemp, currMinTemp, currFeel}
+    }
+    getDay (index=0) {
+        const dayDate = new Date(this.currDate);
+        dayDate.setDate(dayDate.getDate() + index);
+
+        const dayOfWeek = format(dayDate, "E");
+        const icon = this.data.days[index].icon;
+        const maxTemp = this.data.days[0].tempmax;
+        const minTemp = this.data.days[0].tempmin;
+
+        return { dayOfWeek, icon, maxTemp, minTemp}
+    }
+    getDays (len) {
+        let daysList = [];
+        for (let n = 1; n < len + 1; n++) daysList.push(this.getDay(n));
+        return daysList
     }
 }
