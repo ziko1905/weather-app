811c6340d234eb01d0a7596361d63e3be6fc7ff9
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-28 21:48:06 +0200

    Get div specific data for load

--- src/external.js
+++ src/external.js
@@ -3,8 +3,7 @@ export function getWeather(place) {
         fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=7NYE8W9EYLGFYL8YU67GRTJAA`)
         .then(data => data.json())
         .then(response => {
-            console.log(response)
-            resolve()
+            resolve(response)
         })
     })
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import "./styles.css";
 import { getWeather } from "./external";
 import { SearchDiv } from "./load.js"
+import { format } from "date-fns";
 
 SearchDiv.getInput().addEventListener("keypress", (e) => {
-    if (e.key === "Enter") getWeather(SearchDiv.getInputValue())
+    if (e.key === "Enter") loadingController(SearchDiv.getInputValue())
 })
-SearchDiv.getBtn().addEventListener("click", () => getWeather(SearchDiv.getInputValue()))
+SearchDiv.getBtn().addEventListener("click", () => loadingController(SearchDiv.getInputValue()))
+
+async function loadingController (obj) {
+    const data = await getWeather(obj);
+    const obtained = new GetData(data);
+}
+
+class GetData {
+    constructor (data) {
+        this.data = data
+    }
+    getMain () {
+        const address = this.data.resolvedAddress.split(", ") 
+        console.log(address);
+        const title = address[0];
+        const subTitle = address.slice(1).join(", ");
+        const displayDate = format(new Date(), "do MMMM, yyyy")
+        const description = this.data.description;
+        return { title, subTitle, displayDate, description }
+    }
+}
