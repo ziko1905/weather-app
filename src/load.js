d01db2ea166a4483b8038c079d971493bd92cc6e
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-28 02:22:51 +0200

    Have styling for promp page

--- src/load.js
+++ src/load.js
@@ -6,14 +6,43 @@ export function loadDefault() {
     const bg = document.createElement("img");
     const title = document.createElement("h1");
 
-    bg.src = bgImg;
-    bg.className = "bg";
-    title.textContent = "Search weather of:"
-    title.className = "title"
     main.className = "default";
     main.style.zIndex = 1;
+    bg.src = bgImg;
+    bg.className = "bg";
+    title.textContent = "Search weather for:"
+    title.className = "title"
 
     main.appendChild(title);
+    SearchDiv.loadLarge(main);
     document.body.appendChild(bg);
     document.body.appendChild(main);
+}
+
+class SearchDiv {
+    static load() {
+        const searchDiv = document.createElement("div");
+        const searchInput = document.createElement("input");
+        const searchButton = document.createElement("button");
+
+        searchDiv.className = "search-div";
+        searchInput.className = "search-inp";
+        searchButton.className = "search-btn";
+        searchButton.textContent = "Get weather";
+
+        searchDiv.appendChild(searchInput);
+        searchDiv.appendChild(searchButton);
+        return searchDiv
+    }
+
+    static loadSmall(elem) {
+        const div = this.load()
+        div.style.fontSize = "1rem";
+        elem.appendChild(div);
+    }
+    static loadLarge(elem) {
+        const div = this.load()
+        div.style.fontSize = "2rem";
+        elem.appendChild(div);
+    }
 }
